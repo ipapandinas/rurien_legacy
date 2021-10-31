466ae567f12ec49606bbc7181fefd0a206099b97
Igor Papandinas <igor.papandinas@posteo.net>
2021-10-31 18:01:45 +0100

    feat(WorkItem): Modal display added

--- src/components/2-Molecules/WorkItem/WorkItem.tsx
+++ src/components/2-Molecules/WorkItem/WorkItem.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Img, { FluidObject } from 'gatsby-image';
+import { Modal } from '@material-ui/core';
 
 import { useDevice } from '../../../hooks';
 import { categoryNameToValue } from '../../../services';
 import {
+  WORK_MODAL_HEIGHT_SM,
+  WORK_MODAL_WIDTH,
+  WORK_MODAL_WIDTH_SM,
   WORK_SLIDER_HEIGHT_SM,
   WORK_SLIDER_WIDTH,
   WORK_SLIDER_WIDTH_SM,
@@ -41,16 +45,33 @@ export default function WorkItem({ category, setCategory, work }: Props) {
     titulo: title,
   } = work;
   const { isMobile } = useDevice();
+  const [open, setOpen] = useState(false);
 
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const modalHeight = isMobile ? WORK_MODAL_HEIGHT_SM : WORK_MODAL_WIDTH;
+  const modalWidth = isMobile ? WORK_MODAL_WIDTH_SM : WORK_MODAL_WIDTH;
   const sliderHeight = isMobile ? WORK_SLIDER_HEIGHT_SM : WORK_SLIDER_WIDTH;
   const sliderWidth = isMobile ? WORK_SLIDER_WIDTH_SM : WORK_SLIDER_WIDTH;
-  const ratio = media.childImageSharp.fluid.aspectRatio;
-  const height = Number(sliderHeight) / ratio;
-  const width = Number(sliderWidth) * ratio;
 
-  const style = {
-    height: ratio >= 1 ? `${height}px` : `${sliderHeight}px`,
-    width: ratio <= 1 ? `${width}px` : `${sliderWidth}px`,
+  const ratio = media.childImageSharp.fluid.aspectRatio;
+
+  const modalStyleHeight = Number(modalHeight) / ratio;
+  const modalStyleWidth = Number(modalWidth) * ratio;
+  const sliderStyleHeight = Number(sliderHeight) / ratio;
+  const sliderStyleWidth = Number(sliderWidth) * ratio;
+
+  const modalStyle = isMobile
+    ? undefined
+    : {
+        height: ratio >= 1 ? `${modalStyleHeight}px` : `${modalHeight}px`,
+        width: ratio <= 1 ? `${modalStyleWidth}px` : `${modalWidth}px`,
+      };
+
+  const sliderStyle = {
+    height: ratio >= 1 ? `${sliderStyleHeight}px` : `${sliderHeight}px`,
+    width: ratio <= 1 ? `${sliderStyleWidth}px` : `${sliderWidth}px`,
   };
 
   const categoryName =
@@ -70,7 +91,11 @@ export default function WorkItem({ category, setCategory, work }: Props) {
   return (
     <div className="WorkItem">
       {media && (
-        <div className="WorkItem__image" style={style}>
+        <div
+          className="WorkItem__image"
+          style={sliderStyle}
+          onClick={handleOpen}
+        >
           <Img
             className="WorkItem__gatsbyImage"
             alt={title}
@@ -91,6 +116,24 @@ export default function WorkItem({ category, setCategory, work }: Props) {
         </div>
         {!isMobile && isCategoryChip && <CategoryChip />}
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="Agrandar imagen"
+        aria-describedby="Agrandar imagen"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="WorkItem__imageModal" style={modalStyle}>
+          <Img
+            className="WorkItem__gatsbyImage"
+            alt={title}
+            fluid={media.childImageSharp.fluid}
+          />
+        </div>
+      </Modal>
     </div>
   );
 }
