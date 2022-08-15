fe2f858cbe7a4a7cd75b8db6fee391722039aead
Igor Papandinas <igor.papandinas@posteo.net>
2022-08-16 00:19:24 +0200

    feat: Dynamic text in about section

--- src/components/4-Pages/About/About.tsx
+++ src/components/4-Pages/About/About.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 import { useDevice } from '../../../hooks';
+import { useAboutQuery } from '../../../queries';
 
 import { Image } from '../../1-Atoms';
 
 export default function About() {
   const { isMobile } = useDevice();
+  const about = useAboutQuery();
 
   return (
     <div className="About container fade-in">
@@ -24,27 +27,7 @@ export default function About() {
         </span>
       </div>
       <div className="About__content">
-        <p>
-          Licenciada en Periodismo por la Facultad de Ciencias Sociales y de la
-          Comunicación del Pais Vasco. Interesada por la fotografía colaboré con
-          varios diarios de información. Me fui a vivir a Bayona, localidad del
-          sur de Francia, donde realicé durante varios años diferentes cursos de
-          artes plásticas en la Ecole Supérieure d’Art Pays Basque. Allí
-          descubrí la belleza de la pintura al óleo. Actualmente curso el grado
-          de Historia del Arte en la UNED. Miembro de la Asociación Española de
-          Pintores y Escultores, de la Asociación Artística Vizcaína y del
-          colectivo Entre 3.
-        </p>
-        <p>
-          Recreo en mis obras lugares que me rodean, paisajes urbanos,marinos,
-          naturales. Es importante encontrar y elegir el momento, ese instante
-          que mis ojos ven, efímero, parado en el tiempo, atrapado entre
-          pigmentos, quedará intacto, inolvidable. Crear un diálogo entre la
-          obra y su espectador, la pintura y el observador, me fascina. Intento
-          plasmar la realidad, mi realidad, sus colores, mis colores. Quizás, al
-          final, lo que queda reflejado es mi interior. Eso sería fantastico!
-          Así, al observar mi propia obra lograría conocerme un poco más...
-        </p>
+        <ReactMarkdown source={about.texto} />
       </div>
       <div className="About__bottom">
         <div className="About__contact">
